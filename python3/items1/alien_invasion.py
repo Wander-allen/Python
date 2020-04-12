5ff7ef74fd192f762fd789392ec79b224a17b561
Phoenix-dollP <18829353030@163.com>
2020-04-12 20:03:27 +0800

    外星人游戏
    添加记分功能

--- python3/items1/alien_invasion.py
+++ python3/items1/alien_invasion.py
@@ -2,6 +2,9 @@ import sys
 
 import pygame
 from settings import Settings
+from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
@@ -14,19 +17,34 @@ def run_game():
         (ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("alien_invasion")
 
+    #创建play按钮
+    play_button = Button(ai_settings,screen,"Play")
+
     #创建一艘飞船
     ship = Ship(ai_settings,screen)
 
     #创建一个用于存储子弹的编组
     bullets = Group()
-    
+    aliens = Group()
 
+    #创建一个外星人群
+    gf.create_fleet(ai_settings,screen,ship,aliens)
+
+    #创建一个用于存储游戏统计信息的实例
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings,screen,stats)
+
+    
     #开始游戏主循环
     while True:
-        gf.check_events(ai_settings,screen,ship,bullets)
-        ship.update()
-        gf.update_ballets(bullets)
-        gf.update_screen(ai_settings,screen,ship,bullets)
+        gf.check_events(ai_settings,screen,stats,sb,play_button,ship,aliens,bullets)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_ballets(ai_settings,screen,stats,sb,ship,aliens,bullets)
+            gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
+
+        gf.update_screen(ai_settings,stats,sb,screen,ship,aliens,bullets,play_button)
 
 
 run_game()
