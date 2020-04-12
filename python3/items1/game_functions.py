1279177a3879da6ead2d2b10d00e921899a3a630
Phoenix-dollP <18829353030@163.com>
2020-04-12 14:59:14 +0800

    外星人入侵
    设计射击相关类

--- python3/items1/game_functions.py
+++ python3/items1/game_functions.py
@@ -14,7 +14,13 @@ def check_keydown_events(event,ai_settings,screen,ship,bullets):
         ship.moving_left  = True
 
     elif event.key == pygame.K_SPACE:
-        #创建一颗子弹，并将其加入到编组bullets中
+        fire_bullet(ai_settings,screen,ship,bullets)
+
+
+def fire_bullet(ai_settings,screen,ship,bullets):
+    """如果还没有到达限制，就发射一颗子弹"""
+    #创建一颗子弹，并将其加入到编组bullets中
+    if len(bullets) < ai_settings.bullets_allowed:
         new_bullet = Bullet(ai_settings,screen,ship)
         bullets.add(new_bullet)
 
@@ -55,6 +61,16 @@ def update_screen(ai_settings,screen,ship,bullets):
     
     #让最近绘制的屏幕可见
     pygame.display.flip()
+
+def update_ballets(bullets):
+    """更新子弹位置，并删除一小时子弹"""
+    #更新子弹位置
+    bullets.update()
+
+    #删除已消失子弹
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
 
 
 
